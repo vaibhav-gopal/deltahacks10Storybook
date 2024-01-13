f928a6103fde02352ae19ea58d47a0559e980814
Reaper8202 <derek.wu820@gmail.com>
2024-01-13 13:42:26 -0500

    added Cohere and openai

--- backend/audio_transcription.py
+++ backend/audio_transcription.py
@@ -1,3 +1,20 @@
 import cohere
 from google.cloud import speech
-co = cohere.Client('Your API key')
+import openai
+co = cohere.Client('lOF6qWQJ0aYLxWrQwQYYHbUiHIrBWpQXwyVghS1c')
+
+#Cohere Storyboard Generation
+message = "Pretend you are a script writer for a kids TV show, you are tasked with creating a short cartoon story that aims to teach kids about complex concepts. Given these notes create a short story! "
+
+response = co.chat(
+	message, 
+	model="command", 
+	temperature=0.9
+)
+
+answer = response.text
+print(answer)
+
+#GPT Image Generation
+
+openai.api_key = "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"
