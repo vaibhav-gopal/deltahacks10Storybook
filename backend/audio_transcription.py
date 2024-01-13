ed3a87847e974ef41740a47d1063dbb159c259df
Reaper8202 <derek.wu820@gmail.com>
2024-01-13 18:26:28 -0500

    added file output

--- backend/audio_transcription.py
+++ backend/audio_transcription.py
@@ -17,10 +17,21 @@ def generate_story(transcript):
     answer = response.text
     return answer
 
+def generate_flashcards(transcript):
+    message = ("Pretend you are trying to create flashcards to help you remember new concepts for a class, make sure to format each question and answer so that it is organized. Make sure to leave just question and answer, don't add any additional text. Here are the notes for the topic" + transcript)
+
+    response = co.chat(
+        message,
+        model = "command",
+        temperature=0.9
+
+    )
+    answer = response.text
+    return answer
+
 #GPT Image Generation
 def generate_pannels(story):     
     client = OpenAI()
-
     response = client.images.generate(
     model="dall-e-3",
     prompt=("Here is a script for a comic story, generate only images that represent each pannel " + story) ,
@@ -30,13 +41,22 @@ def generate_pannels(story):
     )
 
     image_url = response.data[0].url
+    return image_url
 
+storyboard = generate_story("The Laplace transform is one of many so-called integral transforms in applied mathematics.")
+generate_pannels(storyboard)
 
-output = generate_story("The Laplace transform is one of many so-called integral transforms in applied mathematics.")
+""""
+storyboard = generate_story("The Laplace transform is one of many so-called integral transforms in applied mathematics.")
+flashcards = generate_flashcards("The Laplace transform is one of many so-called integral transforms in applied mathematics.")
 #Uploading output to a txt file
 f = open('storyboard.txt','w')
-f.write(output)
+f.write(storyboard)
 f.close()
 
+s = open('flashcards.txt','w')
+s.write(flashcards)
+s.close()
+"""
 
 
