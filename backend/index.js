18f9363a4fec087bc23ee97a4850a0e2109a5f99
vaibhav-gopal <vabsgop@gmail.com>
2024-01-13 18:00:37 -0500

    updated index

--- backend/index.js
+++ backend/index.js
@@ -6,8 +6,20 @@ const port = process.env.PORT || 8080;
 app.use(express.static(path.join(__dirname, '../frontend/websitestoryboard/build')))
 app.use('/static', express.static(path.join(__dirname, '../frontend/websitestoryboard/public')))
 
+app.get('/flashcards', (req, res) => {
+    res.send('Hello Flashcards!')
+})
 
+app.get('/comics', (req, res) => {
+    res.send('Hello Comics!')
+})
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
 })
+
+
+function getFlashcards() {
+    const spawn = require("child_process").spawn;
+    const pythonProcess = spawn('python',["path/to/script.py", arg1, arg2, ...]);
+}
