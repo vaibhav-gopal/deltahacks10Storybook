edb99d277e8fa7e241c5e2c19a26e7011265f3c1
vaibhav-gopal <vabsgop@gmail.com>
2024-01-13 18:28:47 -0500

    Added images directory

--- backend/index.js
+++ backend/index.js
@@ -5,6 +5,7 @@ const port = process.env.PORT || 8080;
 
 app.use(express.static(path.join(__dirname, '../frontend/websitestoryboard/build')))
 app.use('/static', express.static(path.join(__dirname, '../frontend/websitestoryboard/public')))
+app.use('/images', express.static(path.join(__dirname, 'images')))
 
 app.get('/flashcards', (req, res) => {
     res.send('Hello Flashcards!')
